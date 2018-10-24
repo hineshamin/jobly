c56b1332e26f02beeb83f9dcd7f9a6572bd6fd6d
Peter Farr <Peter@PrismaPhonic.com>
2018-10-24 16:09:36 -0700

    added model for jobs table

--- data.sql
+++ data.sql
@@ -5,4 +5,14 @@ CREATE TABLE companies
   num_employees int,
   description text,
   logo_url text
-)
+);
+
+CREATE TABLE jobs
+(
+  id SERIAL PRIMARY KEY,
+  title text NOT NULL,
+  salary float NOT NULL,
+  equity float NOT NULL CHECK(equity BETWEEN 0 and 1),
+  company_handle text REFERENCES companies,
+  date_posted TIMESTAMP default CURRENT_TIMESTAMP 
+);
