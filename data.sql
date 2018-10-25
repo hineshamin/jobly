94fd96f7449e01a301d9d169e4838b3f2e9da873
Hinesh Amin <hineshamin01@gmail.com>
2018-10-25 11:07:54 -0700

    Added users table

--- data.sql
+++ data.sql
@@ -14,5 +14,17 @@ CREATE TABLE jobs
   salary float NOT NULL,
   equity float NOT NULL CHECK(equity BETWEEN 0 and 1),
   company_handle text REFERENCES companies ON DELETE cascade,
-  date_posted TIMESTAMP default CURRENT_TIMESTAMP 
+  date_posted TIMESTAMP default CURRENT_TIMESTAMP
 );
+
+CREATE TABLE users
+(
+  username text PRIMARY KEY,
+  password text NOT NULL,
+  first_name text NOT NULL,
+  last_name text NOT NULL,
+  email text NOT NULL UNIQUE,
+  photo_url text
+    DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Default_profile_picture_%28male%29_on_Facebook.jpg/600px-Default_profile_picture_%28male%29_on_Facebook.jpg',
+  is_admin boolean NOT NULL default false
+)
